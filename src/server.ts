7ec9175e4832761dfb05f05abcd948f978b00461
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-09-11 11:37:58 +0300

    Bug-fix in /src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import {filterImageFromURL, deleteLocalFiles,isImageUrl} from './util/util';
   const app = express();
 
   // Set the network port
-  const port:number = process.env.PORT || 8082;
+  const port:number|string = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
